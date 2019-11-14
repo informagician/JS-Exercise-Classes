0daff668c4527bc95b650a21954e284fca03335d
Milo Rastgoo <hello@informagician.com>
2019-11-14 13:54:09 -0500

    Task2 constructor and empty methods created

--- index.js
+++ index.js
@@ -74,7 +74,18 @@ class Person {
 */
 
 class Car {
+  constructor(model,milesPerGallon) {
+    this.model= model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank= 0,
+    this.odometer= 0
+  }
+  fill(gallons) {
 
+  }
+  drive(distance) {
+    
+  }
 }
 
 /*
